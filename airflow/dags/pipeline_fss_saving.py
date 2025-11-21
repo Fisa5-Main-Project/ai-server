45895296aa4a5cb27328d39883065d1f5d713086
blueoxygens <exboxer234@gmail.com>
2025-11-21 11:37:34 +0900

    [fix] pipeline mongo db 경로 수정

--- airflow/dags/pipeline_fss_saving.py
+++ airflow/dags/pipeline_fss_saving.py
@@ -1,13 +1,30 @@
 import pendulum
 from airflow.decorators import dag, task
 from airflow.models.variable import Variable
+import os
 
-from etl_utils import fetch_fss_data, transform_deposit_saving, load_to_mongo
+from etl_utils import fetch_fss_data, transform_annuity, load_to_mongo
 
 # --- 0. 설정: Airflow UI의 Variables에서 값 불러오기 ---
+import urllib.parse
+
+# .env 파일이나 환경변수에서 가져옴
+username = os.getenv("MONGO_INITDB_ROOT_USERNAME", "admin")
+password = os.getenv("MONGO_INITDB_ROOT_PASSWORD") 
+
+# 비밀번호에 '@', ':', '/' 같은 문자가 있으면 에러가 나므로 인코딩 필수
+encoded_user = urllib.parse.quote_plus(username)
+encoded_pwd = urllib.parse.quote_plus(password)
+
+# Docker Compose의 service name이 'mongo'라고 가정
+host = "mongo"
+port = "27017"
+
+# ?authSource=admin 은 루트 계정이 admin DB에 생성되기 때문에 붙여주는 게 좋습니다.
+MONGO_DB_URL = f"mongodb://{encoded_user}:{encoded_pwd}@{host}:{port}/?authSource=admin"
 try:
     FSS_API_KEY = Variable.get("FSS_API_KEY")
-    MONGO_DB_URL = Variable.get("MONGO_DB_URL")
+    # MONGO_DB_URL = Variable.get("MONGO_DB_URL")
     MONGO_DB_NAME = Variable.get("DB_NAME")
 except KeyError:
     raise Exception("Airflow Variables에 FSS_API_KEY, MONGO_DB_URL, DB_NAME을 등록해야 합니다.")
